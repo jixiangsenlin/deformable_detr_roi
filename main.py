297b5555a3b4752298952e86975993c07f095a77
jixiangsenlin <48990373+jixiangsenlin@users.noreply.github.com>
2022-08-31 19:10:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from datasets import build_dataset, get_coco_api_from_dataset
 from engine import evaluate, train_one_epoch
 from models import build_model
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Deformable DETR Detector', add_help=False)
@@ -45,6 +45,8 @@ def get_args_parser():
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
                         help='gradient clipping max norm')
 
+    parser.add_argument('--device', default='cuda',#:2
+                        help='device to use for training / testing')
 
     parser.add_argument('--sgd', action='store_true')
 
@@ -118,8 +120,9 @@ def get_args_parser():
 
     parser.add_argument('--output_dir', default='./out/',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
+
+    # parser.add_argument('--device', default='cuda',
+    #                     help='device to use for training / testing')
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
@@ -140,6 +143,7 @@ def main(args):
     print(args)
 
     device = torch.device(args.device)
+    # device = torch.device(args.device if torch.cuda.is_available() else "cpu")
 
     # fix the seed for reproducibility
     seed = args.seed + utils.get_rank()
@@ -329,3 +333,4 @@ if __name__ == '__main__':
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
+
